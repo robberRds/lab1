a696793638d1c9e1a9d435eb31abe57606a09b4a
a0981 <a0981333579as@gmail.com>
2022-10-09 19:57:38 +0300

    Task2

--- javascript.js
+++ javascript.js
@@ -7,3 +7,15 @@ function mathfunction1() {
     document.getElementById('result1').innerHTML = s;
 }
 document.getElementById('bt1').addEventListener('click', mathfunction1);
+
+function mathfunction2() {
+    var n = 1000;
+    var s = 0;
+    for (var i = 0; i < 50; i++) {
+        n = n + n / 40;
+        if (n >= 3000 && s == 0) s = i;
+    }
+    document.getElementById('result2').innerHTML = s;
+}
+
+document.getElementById('bt2').addEventListener('click', mathfunction2);
